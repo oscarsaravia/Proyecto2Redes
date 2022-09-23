217005a3c592c1c1cf5d7eede7fd15874ff55866
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-09-22 21:12:08 -0600

    ADD player component to display name and cards

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,19 @@
+import { Card } from '../Card'
+import './player.scss'
+
+export const Player = ({ player }) => {
+  return (
+    <div className='player-container'>
+      <h2>{player.username}</h2>
+      <div className='player-cards'>
+        {
+          player.cards && player.cards.map((card, index) => (
+            <div key={index}>
+              <Card key={index} type={card} />
+            </div>
+          ))
+        }
+      </div>
+  </div>
+  )
+}
