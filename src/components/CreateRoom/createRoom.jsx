5efab9b29af0a3c7a567e5bbd546893b436de5ae
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-09-22 21:13:42 -0600

    ADD display code and copy btn and states to navigate

--- src/components/CreateRoom/createRoom.jsx
+++ src/components/CreateRoom/createRoom.jsx
@@ -1,20 +1,51 @@
-import './createRoom.css'
+import React, { useState } from 'react';
+import './createRoom.scss'
 import useSession from '../../hooks/useSession'
 import { createRoom } from '../../websockets/socket'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const CreateRoom = () => {
+    const [copied, setCopied] = useState(false)
     const session = useSession()
+    const navigate = useNavigate()
     const { username } = useLocation().state
     const onClick = () => {
-        console.log('Hola', username)
         createRoom(username)
     }
+    const navigateMain = () => {
+        navigate('/game', {
+            state: {
+              username: username,
+              room_id: session.response?.body?.room_id,
+              waiting: false,
+            }
+          });
+    }
+    const copy = () => {
+        navigator.clipboard.writeText(session.response?.body?.room_id)
+        setCopied(true)
+        setTimeout(() => {
+            setCopied(false)
+        }, 1000);
+    }
+
     return (
         <div className="create-room">
             <h1>Crear sala</h1>
             <input type="text" placeholder="Nombre de la sala" className="css-input-room" />
             <button className="myButton-room" onClick={onClick}>Crear sala</button>
+            {
+                session.response?.body?.room_id && (
+                <>
+                    <h2>La sala se ha creado con el id:</h2>
+                    <div className='container'>
+                        <h3>{session.response?.body?.room_id}</h3>
+                        <button className="copy-btn" onClick={copy}>{copied ? 'COPIADO!' : 'COPIAR' }</button>
+                    </div>
+                    <button className="myButton-room" onClick={navigateMain}>START GAME!</button>
+                </>
+                )
+            }
         </div>
     )
 }
