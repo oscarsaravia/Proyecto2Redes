bf0eb13ab0ad366eac3944e510a346615235a203
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-09-24 11:55:29 -0600

    REMOVE player console logs

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -2,7 +2,6 @@ import { Card } from '../Card'
 import './player.scss'
 
 export const Player = ({ player, hide }) => {
-  console.log("Player",player)
   return (
     <div className='player-container'>
       <h2>{player.username}</h2>
