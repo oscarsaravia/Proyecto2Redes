567f352954f269bad261f48b3008ea4e06a8fe37
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-09-24 11:51:13 -0600

    ADD bluff or farol icon

---
+++ src/components/Bluff/bluff.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Fab from '@mui/material/Fab';
+import GavelIcon from '@mui/icons-material/Gavel';
+
+export const Bluff = () => {
+  return (
+    <Fab variant="extended" size="medium" color="primary" aria-label="add">
+      <GavelIcon />
+      </Fab>
+  )
+}
